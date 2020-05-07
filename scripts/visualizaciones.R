1ae5612fb58c9a0119ba4bdaf4a25d8958999a6c
johan-rosa <johan.rosaperez@gmail.com>
2020-05-07 18:44:19 -0400

    mapa todas las provincias

--- scripts/visualizaciones.R
+++ scripts/visualizaciones.R
@@ -9,19 +9,48 @@ url <- "countries/do/do-all"
 
 # Lectura de datos --------------------------------------------------------
 
-infectados <- read_excel("SIR_provincias/infectados2.xlsx")
+# Data de salud pública con los casos acumulados por días
+infectados <- read_excel(
+    "SIR_provincias/Datos Boletines SP - Provincias.xlsx",
+    sheet = "Provincias_CasosConfir", 
+    range = "a1:ah47") %>%
+    # Llevando la data a formato tidy
+    pivot_longer(cols = -Dias, names_to = "provincias", values_to = "infectados")
 
-rcero <- "SIR_provincias/rcero.xlsx" %>% read_excel()
+# Series del Rcero
+rcero <- "SIR_provincias/rcero.xlsx" %>%
+    read_excel() %>% 
+    rename(rcero = infectados)
 
+# Data de los mapas de república domicana en highcharter
 mapdata <- get_data_from_map(download_map_data("countries/do/do-all"))
 
+# highchart catalogo provincia
+catalogo_provincias <- mapdata %>% select(code = `hc-a2`, provincia = `woe-name`)
+
+
+# Manipulando los datos ---------------------------------------------------
+
+# Recoding de las provincias para que coincida con highcharter
+# Ellos tienen algunos nombres con porblemas, pero es necesario
+# que esten iguales para hacer el join
+infectados <- infectados %>% 
+    mutate(
+        provincia_hc = recode(provincias,
+                              "El Seibo" = "El Seybo",
+                              "Bahoruco" = "Baoruco",
+                              "Hermanas Mirabal" = "Hermanas",
+                              "Elías Piña" = "La Estrelleta") 
+    )
+
+
+# Data para el mapa
 mapdata2 <- mapdata %>% 
     left_join(
-        rcero %>% 
-            group_by(code) %>% 
-            filter(fecha == max(fecha)) %>%
-            select(code, infectados),
-        by = c("hc-a2" = "code")) %>% 
+        infectados %>% 
+            group_by(provincias) %>% 
+            filter(Dias == max(Dias)),
+        by = c("woe-name" = "provincia_hc")) %>% 
     select(code = `hc-a2`, infectados) %>% 
     filter(!is.na(infectados))
 
@@ -33,10 +62,11 @@ rcero_nested <- rcero %>%
            iddate = cumsum(iddate)
            ) %>% 
     ungroup() %>% 
-    select(code, iddate, infectados) %>% 
+    select(code, iddate, rcero) %>% 
+    mutate(tocolor = 1) %>% 
     nest(-code) %>% 
     mutate(
-        data = map(data, mutate_mapping, hcaes(x = iddate, y = infectados), drop = TRUE),
+        data = map(data, mutate_mapping, hcaes(x = iddate, y = rcero, color = tocolor), drop = TRUE),
         data = map(data, list_parse)
     ) %>%
     rename(ttdata = data)
@@ -49,7 +79,7 @@ mapdata2 <- left_join(mapdata2, rcero_nested)
 map_rcero <-  hcmap("countries/do/do-all",
       data = mapdata2, joinBy = c("hc-a2", "code"),
       value = "infectados",
-      name = "R cero",
+      name = "Infectados",
       dataLabels = list(enabled = TRUE, format = '{point.name}'),
       borderColor = "#FAFAFA",
       borderwidth = 0.01)
@@ -57,11 +87,12 @@ map_rcero <-  hcmap("countries/do/do-all",
 
 map_rcero <-  map_rcero %>% 
     hc_tooltip(useHTML = TRUE, pointFormatter = tooltip_chart(
-        width = 200,
-        height = 200,
+        width = 300,
+        height = 250,
         accesor = "ttdata",
         hc_opts = list(
             title = list(text = "", useHTML = TRUE),
-            xAxis = list(type = "category")
-        )
+            xAxis = list(type = "category"),
+            series = list(list(color = "midnightblue", name = "Rcero")
+        ))
     ))
